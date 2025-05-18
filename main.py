cd50fd36e4e2374221573ce9d2e1cb9f3131308a
Eternatus9999 <eternatus9999@gmail.com>
2025-05-18 12:35:05 +0530

    Add addition,subtraction,division and multiplication operators with the equal sign and add the mathematic function to the project

--- main.py
+++ main.py
@@ -10,58 +10,88 @@ text = ""
 top = Frame(root)
 bottom = Frame(root)
 
-txtarea = Label(top, width=15, text=text, font=("Arial", 20), bg="lightgrey")
-txtarea.pack()
+textarea = Label(top, width=15, text=text, font=("Arial", 20), bg="lightgrey")
+textarea.pack()
 
 def button_1_click():
     global text
     text += "1"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_2_click():
     global text
     text += "2"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_3_click():
     global text
     text += "3"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_4_click():
     global text
     text += "4"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_5_click():
     global text
     text += "5"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_6_click():
     global text
     text += "6"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_7_click():
     global text
     text += "7"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_8_click():
     global text
     text += "8"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_9_click():
     global text
     text += "9"
-    txtarea.config(text=text)
+    textarea.config(text=text)
 
 def button_0_click():
     global text
     text += "0"
-    txtarea.config(text=text)
+    textarea.config(text=text)
+
+def button_point_click():
+    global text
+    text += "."
+    textarea.config(text=text)
+
+def button_addition():
+    global text
+    text += " + "
+    textarea.config(text=text)
+
+def button_subtract():
+    global text
+    text += " - "
+    textarea.config(text=text)
+
+def button_multiply():
+    global text
+    text += " * "
+    textarea.config(text=text)
+
+def button_divide():
+    global text
+    text += " / "
+    textarea.config(text=text)
+
+def button_equal():
+    global text
+    text = str(eval(text))
+    textarea.config(text=text)
 
 
 btn1 = Button(bottom, text="1", font=("Arial", 20), command=button_1_click)
@@ -74,6 +104,12 @@ btn7 = Button(bottom, text="7", font=("Arial", 20), command=button_7_click)
 btn8 = Button(bottom, text="8", font=("Arial", 20), command=button_8_click)
 btn9 = Button(bottom, text="9", font=("Arial", 20), command=button_9_click)
 btn0 = Button(bottom, text="0", font=("Arial", 20), command=button_0_click)
+btn_point = Button(bottom, text=".", font=("Arial", 20), command=button_point_click)
+btn_add = Button(bottom, text="+", font=("Arial", 20), command=button_addition)
+btn_sub = Button(bottom, text="-", font=("Arial", 20), command=button_subtract)
+btn_mul = Button(bottom, text="*", font=("Arial", 20), command=button_multiply)
+btn_div = Button(bottom, text="/", font=("Arial", 20), command=button_divide)
+btn_equ = Button(bottom, text="=", font=("Arial", 20), command=button_equal)
 
 btn1.grid(column=0, row=1)
 btn2.grid(column=1, row=1)
@@ -85,6 +121,12 @@ btn7.grid(column=0, row=3)
 btn8.grid(column=1, row=3)
 btn9.grid(column=2, row=3)
 btn0.grid(column=0, row=4)
+btn_point.grid(column=1, row=4)
+btn_add.grid(column=3, row=1)
+btn_sub.grid(column=3, row=2)
+btn_mul.grid(column=3, row=3)
+btn_div.grid(column=3, row=4)
+btn_equ.grid(column=2, row=4)
 
 top.pack()
 bottom.pack()
